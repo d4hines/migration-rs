10d136582b1a68676983e43f89492ed20c58cae8
Daniel Hines <d4hines@gmail.com>
2024-02-21 09:14:08 -0500

    wip

--- example_proc_macro/src/lib.rs
+++ example_proc_macro/src/lib.rs
@@ -1,13 +1,12 @@
+use migration_repr::Migratable;
 use migration_repr::Migration;
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use std::collections::BTreeMap;
 use syn::{parse_macro_input, DeriveInput};
-use migration_repr::Migratable;
 
 #[proc_macro]
 pub fn make_migrated_types(_input: TokenStream) -> TokenStream {
-    let expanded = Migration::new(example_legacy_types::LegacyBar::get_ty())
-        .to_token_stream();
+    let expanded = Migration::new(example_legacy_types::LegacyBar::get_ty()).to_token_stream();
     TokenStream::from(expanded)
 }
